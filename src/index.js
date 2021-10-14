6e35ee63f411d92834fcce4c5ebe89dd22d07027
umair-khanzada <umairkz52@gmail.com>
2021-10-14 13:56:34 +0500

    loaded db uri from env.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const config = require('./config/config');
 const logger = require('./config/logger');
 
 let server;
-mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+mongoose.connect(process.env.DB_URI, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
   server = app.listen(config.port, '0.0.0.0', () => {
     logger.info(`Listening to port ${config.port}`);
